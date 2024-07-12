cd8882c31648a9b108cb2ebf33e409ce80696f85
david ben-adeji <benadejidavid@gmail.com>
2024-07-12 12:27:49 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <main>
+      <Card />
+    </main>
+  )
+}
+
+export default App
---
+++ src/util/user.js
@@ -0,0 +1,6 @@
+export const user = {
+  name: "Jessica Randall",
+  location: "London, United, Kingdom",
+  description: "Front-end Developer and avid reader",
+  links: ["Github", "Frontend Mentor", ],
+};
