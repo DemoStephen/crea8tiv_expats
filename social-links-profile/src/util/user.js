9c8cb954f0b160bb1955ec2ef561619f0691f5d5
david ben-adeji <benadejidavid@gmail.com>
2024-08-21 14:01:09 +0100

    created styling_components directory

--- social-links-profile/src/util/user.js
+++ social-links-profile/src/util/user.js
@@ -2,5 +2,5 @@ export const user = {
   name: "Jessica Randall",
   location: "London, United, Kingdom",
   description: "Front-end Developer and avid reader",
-  links: ["Github", "Frontend Mentor", ],
+  links: ["Github", "Frontend Mentor", "Instagram" ],
 };
